771033969eb85680d2166fa8077a5ee36c3738c0
Victoria Samira Ruiz <samiravictoriaruz26@gmail.com>
2026-05-22 15:02:48 -0300

    CasoList.cs modificada

--- Dsw2026Ej11/Collections/CasoList.cs
+++ Dsw2026Ej11/Collections/CasoList.cs
@@ -1,4 +1,7 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using System;
+using System.Collections.Generic;
+using Dsw2026Ej11.Domain;
+namespace Dsw2026Ej11.Collections;
 
 //Crear un campo que represente una lista de alumnos (List<>)
 //Incluir un método para agregar alumnos a la lista
@@ -8,4 +11,26 @@
 //Incluir un método para eliminar un alumno en una determinada posición de la lista
 public class CasoList
 {
+    List<Alumno> alumnos = new List<Alumno>();
+    public void AgregarAlumno(Alumno alumno)
+    {
+        alumnos.Add(alumno);
+    }
+    public List<Alumno> ObtenerAlumnos()
+    {
+        return alumnos;
+    }
+    public Alumno? BuscarAlumno(string nombre)
+    {
+        return alumnos.Find(a => a.Nombre == nombre);
+    }
+    public void EliminarAlumno(Alumno alumno)
+    {
+        alumnos.Remove(alumno);
+    }
+    public void EliminarAlumnoEnPosicion(int posicion)
+    {
+        alumnos.RemoveAt(posicion);
+    }
+
 }
